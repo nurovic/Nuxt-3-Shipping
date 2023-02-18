6513051545de4d54bf5f8d7c9fb4362c6f99af9c
nurovic <mertnurovic@gmail.com>
2023-02-18 23:24:35 +0300

    initialize project

---
+++ pages/course.vue
@@ -0,0 +1,29 @@
+<template lang="">
+    <div class="bg-gray-100 p-12 w-full h-full  min-h-screen flex flex-col items-center">
+        <div class="prose mb-12">
+            <h1>
+                <span class="font-bold text-2xl">
+                    Companies & Products
+                </span>
+            </h1>
+        </div>
+       <div class="left flex flex-row justify-center flex-grow">
+        <div class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col">
+            <h3 class="font-bold ">COMPANIES</h3>
+        </div>
+
+
+        <div class="right prose p-8 bg-white rounded rounded-md w-[65ch]">
+            <h3 class="font-bold ">Products INFO</h3>
+        </div>
+       </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
