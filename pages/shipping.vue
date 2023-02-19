e72ed202e162af33f2aca58f12b4cdd5fca8b999
nurovic <mertnurovic@gmail.com>
2023-02-19 15:08:44 +0300

    Made dynamic data

--- pages/shipping.vue
+++ pages/shipping.vue
@@ -7,9 +7,32 @@
                 </span>
             </h1>
         </div>
-       <div class="left flex flex-row justify-center flex-grow">
-        <div class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col">
-            <h3 class="font-bold ">COMPANIES</h3>
+       <div class="left flex flex-row justify-center flex-grow h-96">
+        <div class="prose mr-4 p-8 bg-white overflow-y-scroll  rounded-md min-w-[20ch] flex flex-col">
+            <h3 class="font-bold mb-6">COMPANIES</h3>
+            <div
+            class="space-y-1 mb-4 flex flex-col"
+            v-for="company in companies"
+            :key="company.id"
+            >
+            <div class="font-bold underline">{{company.Company}}</div>
+            <nuxt-link
+            v-for="(product, index) in company.products"
+            :key="product.id"
+            class="flex flex-row space-x-1 no-underline prose-sm font-normal"
+            :to="product.path"
+            :class="{
+                'text-blue-500 font-semibold':
+                product.path === $route.fullPath,
+                'text-gray-600':
+                product.path !== $route.fullPath  
+            }"
+            >
+            <span class="text-gray-500">{{index + 1}}.</span>
+            <span>{{product.ProductName}}</span>
+
+            </nuxt-link> 
+            </div>
         </div>
 
 
@@ -19,11 +42,8 @@
        </div>
     </div>
 </template>
-<script>
-export default {
-    
-}
+<script setup>
+const { companies } = useShipping()
 </script>
-<style lang="">
-    
+<style >
 </style>
