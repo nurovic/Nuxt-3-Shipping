73c5f4fe278589f18fd2c5f1921bfeeabe5c229f
nurovic <mertnurovic@gmail.com>
2023-02-19 15:09:13 +0300

    Change composables

--- composables/useShipping.ts
+++ composables/useShipping.ts
@@ -1,5 +1,15 @@
 import shippingData from '../shippingData'
+console.log("alkdaskjd", shippingData)
 
 export const useShipping = () => {
-    return shippingData
+    return {
+        ...shippingData,
+        companies: shippingData.companies.map((company) => ({
+            ...company,
+            products: company.Product.map((item) => ({
+                ...item,
+                path:`/shipping/company/${company.id}/product/${item.id}`
+            }))
+        }))
+    }
 }
