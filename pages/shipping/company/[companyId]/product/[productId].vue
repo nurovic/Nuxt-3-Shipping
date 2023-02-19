e434ea9256a4a88943661fc1590d593c7953ff43
nurovic <mertnurovic@gmail.com>
2023-02-19 15:08:30 +0300

    Changed style

--- pages/shipping/company/[companyId]/product/[productId].vue
+++ pages/shipping/company/[companyId]/product/[productId].vue
@@ -1,26 +1,45 @@
 <template lang="">
-    <div>
-        <h3 class="font-bold ">Product</h3>
-        <p>Company id: {{company.Business}}</p>
-        <p>Product id: {{product.City}}</p>
+  <div>
+    <h3 class="font-bold mb-4 text-gray-600">Product</h3>
+    <div class="flex">
+      <div class="font-bold mr-2">Product:</div>
+      {{ product.Product }}
     </div>
+    <div class="flex">
+      <div class="font-bold mr-2">Daily Destination:</div>
+      {{ product.DailyDestination }}
+    </div>
+    <div class="flex">
+      <div class="font-bold mr-2">Vehicle:</div>
+      {{ product.Vehicle }}
+    </div>
+    <div class="flex">
+      <div class="font-bold mr-2">Driver:</div>
+      {{ product.Driver }}
+    </div>
+    <div class="flex">
+      <div class="font-bold mr-2">City:</div>
+      {{ product.City }}
+    </div>
+    <div class="flex">
+        <div class="font-bold mr-2">Description:</div> 
+        {{ product.Description }}</div>
+  </div>
 </template>
 <script setup>
-const shipping = useShipping()
-const route = useRoute()
-console.log(shipping)
+const shipping = useShipping();
+console.log("İD", shipping)
+const route = useRoute();
+console.log(shipping);
 const company = computed(() => {
-    return shipping.companies.find(
-        (company) => company.id === route.params.companyId
-    )
-})
+  return shipping.companies.find(
+    (company) => company.id === route.params.companyId
+  );
+});
 const product = computed(() => {
-    return company.value.Transportation.find(
-        (product) => product.id === route.params.productId
-    )
-})
-
+  return company.value.Product.find(
+    (product) => product.id === route.params.productId
+  );
+});
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
