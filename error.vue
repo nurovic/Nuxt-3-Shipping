627944925754aac0f6efdc5ad1768058e1d7d4a7
nurovic <mertnurovic@gmail.com>
2023-02-22 14:25:08 +0300

    Added Error Page

---
+++ error.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="h-screen flex flex-col justify-center items-center">
+        <p class="text-2xl font-semibold mb-4">
+            This Page Not Found 404
+        </p>
+        <nuxt-link to="/" class="text-xl font-bold border-2 rounded-xl px-4">Back Home</nuxt-link>
+    </div>
+</template>
